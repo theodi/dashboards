98a789e5876e177a9bee6bc54992b38d59e5b3ee
James Smith <james@floppy.org.uk>
2013-04-18 16:02:53 +0100

    Move event list updating into a separate lib to enable better testing

--- jobs/event_list.rb
+++ jobs/event_list.rb
@@ -1,14 +1,5 @@
-require 'net/http'
-require 'active_support/core_ext/date/conversions'
-
 SCHEDULER.every '1h', :first_at => Time.now do
+  
+  send_event('event-list', EventList.update )
 
-  json = Net::HTTP.get URI.parse("http://www.theodi.org/sites/default/files/courses.json")
-  events = JSON.parse(json)
-
-  event_data = events.map do |url, data|
-    {url: url, text: "event name (#{Date.parse(data['startDate']).to_formatted_s(:short)})" }
-  end
-
-  send_event('event-list', { items: event_data })
 end
---
+++ lib/event_list.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'active_support/core_ext/date/conversions'
+
+class EventList
+  
+  def self.update
+    
+    json = Net::HTTP.get URI.parse("http://www.theodi.org/sites/default/files/courses.json")
+    events = JSON.parse(json)
+
+    event_list = events.map do |url, data|
+      {url: url, text: "event name (#{Date.parse(data['startDate']).to_formatted_s(:short)})" }
+    end
+
+    { items: event_list }
+
+  end
+  
+end
