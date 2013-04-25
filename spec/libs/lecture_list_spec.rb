9c9cdee15df4640bc787f6eafd3edc4b19792a2a
pezholio <pezholio@gmail.com>
2013-04-25 20:27:56 +0100

    Sort the tests

--- spec/libs/lecture_list_spec.rb
+++ spec/libs/lecture_list_spec.rb
@@ -5,9 +5,8 @@ describe LectureList do
   it "should load lectures from JSON and create list", :vcr do
     lecture_data = LectureList.update
     lecture_data[:items].size.should == 5
-    lecture_data[:items].first[:text].should == "Friday Lunchtime Lectures: Big Data Week Special - Data as Culture (26 Apr)"
+    lecture_data[:items].first[:text].should == "Friday Lunchtime Lectures: Big Data Week Special - Data as Culture"
+    lecture_data[:items].first[:date].should == "26 Apr"
     lecture_data[:items].first[:url].should == "http://www.eventbrite.com/event/6153638699"
-    lecture_data[:items].first[:tickets].should == 1
-    lecture_data[:items].first[:capacity].should == 100
   end
 end
