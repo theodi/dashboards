110e02464cbe4cd458bc67100cb3922e8ada2146
pezholio <pezholio@gmail.com>
2013-04-29 09:38:44 +0100

    Fix the tests

--- spec/libs/event_list_spec.rb
+++ spec/libs/event_list_spec.rb
@@ -4,8 +4,8 @@ require 'event_list'
 describe EventList do
   it "should load events from JSON and create list", :vcr do
     event_data = EventList.update
-    event_data[:items].size.should == 5
-    event_data[:items].first[:text].should == " (26 Apr)"
-    event_data[:items].first[:url].should == "http://www.eventbrite.com/event/6153638699"
+    event_data[:items].size.should == 4
+    event_data[:items].first[:text].should == " ( 3 May)"
+    event_data[:items].first[:url].should == "http://www.eventbrite.com/event/6190416703"
   end
 end
