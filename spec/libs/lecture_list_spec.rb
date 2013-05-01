218314fbbcc21b54ad186b2f58f3befac5524091
pezholio <pezholio@gmail.com>
2013-05-01 10:52:35 +0100

    That's better

--- spec/libs/lecture_list_spec.rb
+++ spec/libs/lecture_list_spec.rb
@@ -4,7 +4,7 @@ require 'lecture_list'
 describe LectureList do
   it "should load lectures from JSON and create list", :vcr do
     lecture_data = LectureList.update("Live")
-    lecture_data[:items].size.should == 100
+    lecture_data[:items].size.should == 5
     lecture_data[:items].first[:text].should == "Friday lunchtime lectures: Will open data give citizens super powers?"
     lecture_data[:items].first[:date].should == "24 May"
     lecture_data[:items].first[:url].should == "http://www.eventbrite.com/event/6211220929"
