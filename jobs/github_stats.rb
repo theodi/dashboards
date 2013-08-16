ddec4b3870986641e4cd9c5478585702588ef429
pezholio <pezholio@gmail.com>
2013-08-16 15:20:55 +0100

    Tweak github stats stuff so it actually works

--- jobs/github_stats.rb
+++ jobs/github_stats.rb
@@ -3,10 +3,10 @@ SCHEDULER.every '1h', :first_at => Time.now do
   issues = GithubDashboard.issues
   external_pulls = GithubDashboard.externalpulls
   
-  send_event('pull-requests', issues[:pull_requests] )
-  send_event('issues', issues[:open_issues] )
-  send_event('external-pulls', external_pulls[:total_pulls] )
-  send_event('external-pulls-merged', external_pulls[:merged_pulls] )
+  send_event('pull-requests', { current: issues[:pull_requests] })
+  send_event('issues-graph', { current: issues[:open_issues] } )
+  send_event('external-pulls', { current: external_pulls[:total_pulls] } )
+  send_event('external-pulls-merged', { current: external_pulls[:merged_pulls] } )
   send_event('milestone', GithubDashboard.milestone )
   
   
