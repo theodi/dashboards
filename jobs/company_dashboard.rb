8dbd1c14744a19a98c462dcfcd81dd552d31f592
pezholio <pezholio@gmail.com>
2013-09-25 17:37:25 +0100

    Add suffixes and prefixes as needed

--- jobs/company_dashboard.rb
+++ jobs/company_dashboard.rb
@@ -1,3 +1,6 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 SCHEDULER.every '1h', :first_at => Time.now do
   progress = CompanyDashboard.progress
   send_event('q1-progress', { min: 0, max: 100, value: progress[:q1] })
@@ -8,9 +11,9 @@ end
 
 SCHEDULER.every '5m', :first_at => Time.now do
   send_event('Reach', { current: CompanyDashboard.reach })
-  send_event('Bookings', { current: CompanyDashboard.bookings })
+  send_event('Bookings', { current: CompanyDashboard.bookings, prefix: "£" })
   send_event('Members', { current: CompanyDashboard.members })
-  send_event('Value', { current: CompanyDashboard.value })
+  send_event('Value', { current: CompanyDashboard.value, prefix: "£" })
   send_event('ODCs', { current: CompanyDashboard.odcs })
-  send_event('KPIs', { current: CompanyDashboard.kpis })
+  send_event('KPIs', { current: CompanyDashboard.kpis, suffix: "%" })
 end
