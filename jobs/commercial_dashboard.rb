534474d27a54cb40a07f2b69d2cdebe91557eded
pezholio <pezholio@gmail.com>
2013-12-16 10:12:43 +0000

    Add suffix to average age

--- jobs/commercial_dashboard.rb
+++ jobs/commercial_dashboard.rb
@@ -1,5 +1,9 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 SCHEDULER.every '1h', :first_at => Time.now do
   send_event('members', { current: CommercialDashboard.members })
-  send_event('age', { current: CommercialDashboard.age })
+  send_event('age', { current: CommercialDashboard.age, suffix: " days" })
   send_event('old-opportunities', { current: CommercialDashboard.old_opportunities })
+  send_event('pipeline', { current: CommercialDashboard.pipeline, prefix: "£" })
 end
