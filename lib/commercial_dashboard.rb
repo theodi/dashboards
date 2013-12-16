c99c36b0b686693efdd1d5c266d390fbe2ba7c22
pezholio <pezholio@gmail.com>
2013-12-16 08:45:28 +0000

    Skeleton methods for all the metrics

---
+++ lib/commercial_dashboard.rb
@@ -0,0 +1,41 @@
+require 'metrics_helper'
+
+class CommercialDashboard < MetricsHelper
+    
+  def self.pipeline
+    
+  end
+  
+  def self.weighted
+    
+  end
+  
+  def self.three_year
+    
+  end
+  
+  def self.bookings
+  
+  end
+  
+  def self.revenue
+    
+  end
+  
+  def self.backlog
+    
+  end
+  
+  def self.members
+
+  end
+  
+  def self.age
+    
+  end
+  
+  def self.old_opportunities
+    
+  end
+  
+end
