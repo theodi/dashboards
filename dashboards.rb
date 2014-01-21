a45d87539545e7ebd425a62c358e9d0b02bbbfb2
pikesley <github@orgraphone.org>
2014-01-21 17:17:04 +0000

    Refactor this so it's includable in cucumber

---
+++ dashboards.rb
@@ -0,0 +1,18 @@
+require 'dashing'
+require 'dotenv'
+
+Dotenv.load
+
+configure do
+  set :auth_token, 'YOUR_AUTH_TOKEN'
+  set :root_dashboard, 'company'
+
+  helpers do
+    def protected!
+     # Put any authentication code you want in here.
+     # This method is run before accessing any resource.
+    end
+  end
+end
+
+
