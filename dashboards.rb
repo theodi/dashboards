a41445ea0c26ab379f4b22556a3ab6005ebb168b
James Smith <james@floppy.org.uk>
2014-01-22 14:38:39 +0000

    Put 2013 dashboard back on root
    for now, until there are some numbers in 2014

--- dashboards.rb
+++ dashboards.rb
@@ -5,7 +5,7 @@ Dotenv.load
 
 configure do
   set :auth_token, 'YOUR_AUTH_TOKEN'
-  set :default_dashboard, '2014_company'
+  set :default_dashboard, '2013_company'
 
   helpers do
     def protected!
