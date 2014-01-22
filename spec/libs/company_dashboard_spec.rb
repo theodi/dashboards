4b354bd6840a956d91008fb196ccd5296fa2d84b
James Smith <james@floppy.org.uk>
2014-01-22 14:21:06 +0000

    Fix tests

--- spec/libs/company_dashboard_spec.rb
+++ spec/libs/company_dashboard_spec.rb
@@ -4,7 +4,7 @@ require 'company_dashboard'
 describe CompanyDashboard do
   
   it "should show correct progress for each quarter", :vcr do
-    progress = CompanyDashboard.progress
+    progress = CompanyDashboard.progress(2013)
     
     progress[:q1].should == 97
     progress[:q2].should == 90.2
@@ -13,27 +13,27 @@ describe CompanyDashboard do
   end
   
   it "should show the correct number of published Open Data Certificates", :vcr do
-    CompanyDashboard.odcs.should == "629"
+    CompanyDashboard.odcs(2013).should == "629"
   end
   
   it "should show the correct member count", :vcr do
-    CompanyDashboard.members.should == 57
+    CompanyDashboard.members(2013).should == 57
   end
   
   it "should show the correct reach", :vcr do
-    CompanyDashboard.reach.should == "303396"
+    CompanyDashboard.reach(2013).should == "303396"
   end
   
   it "should show the correct bookings value", :vcr do
-    CompanyDashboard.bookings.to_i.should == 2191064
+    CompanyDashboard.bookings(2013).to_i.should == 2191064
   end
   
   it "should show the correct unlocked value", :vcr do
-    CompanyDashboard.value.to_i.should == 16924307
+    CompanyDashboard.value(2013).to_i.should == 16924307
   end
   
   it "should show the correct kpi percentage", :vcr do
-    CompanyDashboard.kpis.should == 100.0
+    CompanyDashboard.kpis(2013).should == 100.0
   end
   
 end
