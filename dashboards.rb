06c8ba38f65d02a12a7c89216661b1ef19e3dc36
James Smith <james@floppy.org.uk>
2014-01-24 11:39:51 +0000

    Move company dashboards into subdirectories

--- dashboards.rb
+++ dashboards.rb
@@ -5,7 +5,7 @@ Dotenv.load
 
 configure do
   set :auth_token, 'YOUR_AUTH_TOKEN'
-  set :default_dashboard, 'company_2013'
+  set :default_dashboard, 'company/2013'
 
   helpers do
     def protected!
