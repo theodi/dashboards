e0acf390f779cb74637ada1ee9d68a567f592abe
pezholio <pezholio@gmail.com>
2014-04-08 13:56:20 +0100

    Send jobs to all the widgets

--- jobs/progress.rb
+++ jobs/progress.rb
@@ -1,7 +1,19 @@
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
-  q2_progress = Progress.new("sFETRDq0")
-  send_event('2014-q2-current-month', items: q2_progress.current_month)
-  send_event('2014-q2-rest-of-quarter', items: q2_progress.rest_of_quarter)
-  send_event('2014-q2-discuss', items: q2_progress.to_discuss)
-  send_event('2014-q2-done', items: q2_progress.done)
+  boards = {
+    "2013-q1" => "cEwY2JHh",
+    "2013-q2" => "m5Gxybf6",
+    "2013-q3" => "wkIzhRE3",
+    "2013-q4" => "5IZH6yGG",
+    "2014-q1" => "8P2Hgzlh",
+    "2014-q2" => "sFETRDq0"
+  }
+
+  boards.each do |k,v|
+    progress = Progress.new(v)
+    send_event("#{k}-current-month", items: progress.current_month)
+    send_event("#{k}-rest-of-quarter", items: progress.rest_of_quarter)
+    send_event("#{k}-discuss", items: progress.to_discuss)
+    send_event("#{k}-done", items: progress.done)
+  end
+
 end
