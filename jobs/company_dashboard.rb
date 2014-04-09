a6dcd9cb4cc8d26e9fe47244fa5a778cad5f133f
James Smith <james@floppy.org.uk>
2014-04-09 13:33:56 +0100

    make currency specifier explicit rather than using prefix

--- jobs/company_dashboard.rb
+++ jobs/company_dashboard.rb
@@ -9,10 +9,10 @@ SCHEDULER.every '1h', :first_in => Time.now + 10 do
   # 2013 Company
   send_event '2013-Reach',    current: CompanyDashboard.reach(2013)
   send_event '2013-Members',  current: CompanyDashboard.members(2013), link: "http://directory.theodi.org/members"
-  send_event '2013-Value',    current: CompanyDashboard.value(2013),   prefix: "£"
+  send_event '2013-Value',    current: CompanyDashboard.value(2013),   currency: "GBP"
   send_event '2013-ODCs',     current: CompanyDashboard.odcs(2013),    link: "https://certificates.theodi.org/status"
   send_event '2013-KPIs',     current: CompanyDashboard.kpis(2013),    suffix: "%"
-  send_event '2013-Bookings', current: CompanyDashboard.old_bookings(2013), prefix: "£" # derprecated metric
+  send_event '2013-Bookings', current: CompanyDashboard.old_bookings(2013), currency: "GBP" # derprecated metric
   # Trello gubbins
   progress = CompanyDashboard.progress(2013)
   send_event '2013-q1-progress', min: 0, max: 100, value: progress[:q1]
@@ -24,10 +24,10 @@ end
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
   # 2014 Company
   send_event '2014-Reach', current: CompanyDashboard.reach(2014), link: "/reach/2014"
-  send_event '2014-Value', current: CompanyDashboard.value(2014), prefix: "£"
-  send_metric_with_targets '2014-commercial-bookings',     CompanyDashboard.commercial_bookings(2014),    link: "/research-projects-training/2014", prefix: "£"
-  send_metric_with_targets '2014-non-commercial-bookings', CompanyDashboard.noncommercial_bookings(2014), link: "/research-projects-training/2014", prefix: "£"
-  send_metric_with_targets '2014-grant-funding',           CompanyDashboard.grant_funding(2014),          prefix: "£"
+  send_event '2014-Value', current: CompanyDashboard.value(2014), currency: "GBP"
+  send_metric_with_targets '2014-commercial-bookings',     CompanyDashboard.commercial_bookings(2014),    link: "/research-projects-training/2014", currency: "GBP"
+  send_metric_with_targets '2014-non-commercial-bookings', CompanyDashboard.noncommercial_bookings(2014), link: "/research-projects-training/2014", currency: "GBP"
+  send_metric_with_targets '2014-grant-funding',           CompanyDashboard.grant_funding(2014),          currency: "GBP"
   send_metric_with_targets '2014-network-size',            CompanyDashboard.network_size(2014),           link: "/network/2014"
   data = []
   CompanyDashboard.bookings_by_sector(2014).each do |k, v|
@@ -60,23 +60,23 @@ end
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
   # 2014 Research, Projects & Training board
   bookings_by_sector = CompanyDashboard.bookings_by_sector(2014)
-  send_metric_with_targets '2014-Commercial-research',     bookings_by_sector['research']['commercial'], prefix: "£"
-  send_metric_with_targets '2014-Commercial-training',     bookings_by_sector['training']['commercial'], prefix: "£"
-  send_metric_with_targets '2014-Commercial-projects',     bookings_by_sector['projects']['commercial'], prefix: "£"
-  send_metric_with_targets '2014-Non-commercial-research', bookings_by_sector['research']['non_commercial'], prefix: "£"
-  send_metric_with_targets '2014-Non-commercial-training', bookings_by_sector['training']['non_commercial'], prefix: "£"
-  send_metric_with_targets '2014-Non-commercial-projects', bookings_by_sector['projects']['non_commercial'], prefix: "£"
+  send_metric_with_targets '2014-Commercial-research',     bookings_by_sector['research']['commercial'], currency: "GBP"
+  send_metric_with_targets '2014-Commercial-training',     bookings_by_sector['training']['commercial'], currency: "GBP"
+  send_metric_with_targets '2014-Commercial-projects',     bookings_by_sector['projects']['commercial'], currency: "GBP"
+  send_metric_with_targets '2014-Non-commercial-research', bookings_by_sector['research']['non_commercial'], currency: "GBP"
+  send_metric_with_targets '2014-Non-commercial-training', bookings_by_sector['training']['non_commercial'], currency: "GBP"
+  send_metric_with_targets '2014-Non-commercial-projects', bookings_by_sector['projects']['non_commercial'], currency: "GBP"
 end
 
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
   # 2014 OpExs
-  send_metric_with_targets '2014-Income',      CompanyDashboard.income(2014),  prefix: "£"
+  send_metric_with_targets '2014-Income',      CompanyDashboard.income(2014),  currency: "GBP"
   send_event '2014-Headcount',        current: CompanyDashboard.headcount(2014)['actual']
-  send_event '2014-EBITDA-YTD',       current: CompanyDashboard.ebitda(2014)['actual'], prefix: "£"
-  send_event '2014-EBITDA',           current: CompanyDashboard.ebitda(2014)['latest'], prefix: "£"
-  send_metric_with_targets '2014-Total-Costs', CompanyDashboard.total_costs(2014), prefix: "£"
-  send_event '2014-Burn',  current:            CompanyDashboard.burn(2014), prefix: "£"
-  send_event '2014-Cash-Reserves',  current:   CompanyDashboard.cash_reserves, prefix: "£"
+  send_event '2014-EBITDA-YTD',       current: CompanyDashboard.ebitda(2014)['actual'], currency: "GBP"
+  send_event '2014-EBITDA',           current: CompanyDashboard.ebitda(2014)['latest'], currency: "GBP"
+  send_metric_with_targets '2014-Total-Costs', CompanyDashboard.total_costs(2014), currency: "GBP"
+  send_event '2014-Burn',  current:            CompanyDashboard.burn(2014), currency: "GBP"
+  send_event '2014-Cash-Reserves',  current:   CompanyDashboard.cash_reserves, currency: "GBP"
   pie = CompanyDashboard.fixed_cost_breakdown(2014).map do |key, value|
     {label: key.humanize, value: value}
   end
@@ -86,9 +86,9 @@ end
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
   # Lifetime
   send_event 'Lifetime-Reach',          current: CompanyDashboard.reach
-  send_event 'Lifetime-Value',          current: CompanyDashboard.value,   prefix: "£"
+  send_event 'Lifetime-Value',          current: CompanyDashboard.value,   currency: "GBP"
   send_event 'Lifetime-ODCs',           current: CompanyDashboard.odcs,    link: "https://certificates.theodi.org/status"
   send_event 'Lifetime-network-size',   current: CompanyDashboard.network_size
   send_event 'Lifetime-people-trained', current: CompanyDashboard.people_trained
-  send_event 'Lifetime-bookings',       current: CompanyDashboard.cumulative_bookings, prefix: "£"
+  send_event 'Lifetime-bookings',       current: CompanyDashboard.cumulative_bookings, currency: "GBP"
 end
