cd9a8aa6fa95e6acad849f92493925ecadd6d8bc
James Smith <james@floppy.org.uk>
2014-05-13 13:52:35 +0100

    activesupport include fix

--- lib/membership_dashboard.rb
+++ lib/membership_dashboard.rb
@@ -1,27 +1,27 @@
-require 'active_support/inflector'
+require 'active_support/all'
 require_relative 'metrics_helper'
 
 class MembershipDashboard < MetricsHelper
-    
+
   def self.count
     response = load_metric("membership-count")
     response["value"]["total"]
   end
-  
+
   def self.by_level
     response = load_metric("membership-count")
-    
+
     levels = {}
     response["value"]["by_level"].each { |k,v| levels[k] = ((v.to_i / response["value"]["total"].to_f)*100).to_i.to_s }
-    
+
     levels
   end
-  
+
   def self.renewals
     response = load_metric("membership-renewals")
-        
+
     one_month, quarter, six_months = [], [], []
-    
+
     response["value"].each do |cat,metrics|
       metrics.each do |k,v|
         metric = { label: k.capitalize.pluralize, value: v }
@@ -35,12 +35,12 @@ class MembershipDashboard < MetricsHelper
         end
       end
     end
-    
-    [ 
+
+    [
       {:title => "Next month", :items => one_month.compact },
       {:title => "Next quarter", :items => quarter.compact },
       {:title => "Next six months", :items => six_months.compact }
     ]
   end
-  
+
 end
