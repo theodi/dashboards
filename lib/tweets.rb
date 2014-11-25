8ba9594c86d7a0c959f5fba2f7ef3496848e21f5
James Smith <james@floppy.org.uk>
2014-11-25 14:47:57 +0000

    Fix twitter

--- lib/tweets.rb
+++ lib/tweets.rb
@@ -6,19 +6,22 @@ class Tweets
     tweets = connection.mentions_timeline(:count => 3)
     if tweets
       tweets.map! do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_uri_https.to_s }
       end
     end
   
     tweets
+  rescue Twitter::Error::TooManyRequests
+    puts "rate limited by twitter"
+    []
   end
   
   def self.connection
-    Twitter.configure do |config|
+    Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_UKODITECH_CONSUMER_KEY']
       config.consumer_secret = ENV['TWITTER_UKODITECH_CONSUMER_SECRET']
-      config.oauth_token = ENV['TWITTER_UKODITECH_OAUTH_TOKEN']
-      config.oauth_token_secret = ENV['TWITTER_UKODITECH_OAUTH_TOKEN_SECRET']
+      config.access_token = ENV['TWITTER_UKODITECH_OAUTH_TOKEN']
+      config.access_token_secret = ENV['TWITTER_UKODITECH_OAUTH_TOKEN_SECRET']
     end
   end
   
