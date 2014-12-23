2fcab4cac8da9d438b1a5cb1eaac53459624e022
James Smith <james@floppy.org.uk>
2014-12-23 11:10:14 +0000

    Slow down travis a bit

--- jobs/travis_builds.rb
+++ jobs/travis_builds.rb
@@ -1,4 +1,4 @@
-SCHEDULER.every '10s', :first_in => Time.now + 10 do
+SCHEDULER.every '30s', :first_in => Time.now + 10 do
 
   send_event('travis-builds', TravisBuilds.update)
   send_event('travis-build-image', TravisBuilds.build_images)
