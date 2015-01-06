19a56193e60edcfb1b80e93c0f5985dc24bb05be
James Smith <james@floppy.org.uk>
2015-01-06 18:12:04 +0000

    fix tests that failed because IT'S THE FUTURE NOW

--- spec/libs/commercial_dashboard_spec.rb
+++ spec/libs/commercial_dashboard_spec.rb
@@ -3,6 +3,10 @@ require 'commercial_dashboard'
 
 describe CommercialDashboard do
   
+  before :all do
+    Timecop.freeze(2014,5,1)
+  end
+  
   it "should show the pipeline for the current year", :vcr do
     CommercialDashboard.pipeline.should == 470825
   end
@@ -25,6 +29,10 @@ describe CommercialDashboard do
   
   it "should show a count of old opportunities", :vcr do
     CommercialDashboard.old_opportunities.should == 107
+  end
+  
+  after :all do
+    Timecop.return
   end
   
 end
