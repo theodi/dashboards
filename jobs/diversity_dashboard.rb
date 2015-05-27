aa6be0d784b5540f8262350082bfb1453199fd1b
Andy Lulham <a.lulham@gmail.com>
2015-05-27 01:35:55 +0100

    Diversity dashboard updates.
    Assuming core is operations, and innovation is technical.

--- jobs/diversity_dashboard.rb
+++ jobs/diversity_dashboard.rb
@@ -4,9 +4,8 @@
 SCHEDULER.every '1h', :first_in => Time.now + 10 do
   send_event 'Diversity-gender-total', value: DiversityDashboard.gender_total
   send_event 'Diversity-gender-board', value: DiversityDashboard.gender_board
-  send_event 'Diversity-gender-smt', value: DiversityDashboard.gender_smt
-  send_event 'Diversity-gender-commercial', value: DiversityDashboard.gender_commercial
+  send_event 'Diversity-gender-leadership', value: DiversityDashboard.gender_leadership
   send_event 'Diversity-gender-international', value: DiversityDashboard.gender_international
-  send_event 'Diversity-gender-operations', value: DiversityDashboard.gender_operations
-  send_event 'Diversity-gender-technical', value: DiversityDashboard.gender_technical
+  send_event 'Diversity-gender-core', value: DiversityDashboard.gender_core
+  send_event 'Diversity-gender-innovation', value: DiversityDashboard.gender_innovation
 end
