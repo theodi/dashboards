265b4141a326ad6e33dcdf7fda89ce4553a4056c
James Smith <james@floppy.org.uk>
2016-07-05 18:11:15 +0100

    It's 2016 duh

--- jobs/news_articles_dashboard.rb
+++ jobs/news_articles_dashboard.rb
@@ -2,43 +2,43 @@
 # encoding: utf-8
 
 schedule do
-  send_event 'comms-spokespeople', items: NewsArticlesDashboard.spokespeople(2014).first(10)
+  send_event 'comms-spokespeople', items: NewsArticlesDashboard.spokespeople(2016).first(10)
 end
 
 schedule do
-  send_event 'comms-sectors', items: NewsArticlesDashboard.sectors(2014).first(10)
+  send_event 'comms-sectors', items: NewsArticlesDashboard.sectors(2016).first(10)
 end
 
 schedule do
-  send_event 'comms-geographies', items: NewsArticlesDashboard.geographies(2014).first(22)
+  send_event 'comms-geographies', items: NewsArticlesDashboard.geographies(2016).first(22)
 end
 
 schedule do
-  send_event 'comms-sentiment', value: NewsArticlesDashboard.sentiment(2014), colours: ['#67EF67','#F9BC26','#ff6700','#D60303'], width: 240, height: 200, radius: 100
+  send_event 'comms-sentiment', value: NewsArticlesDashboard.sentiment(2016), colours: ['#67EF67','#F9BC26','#ff6700','#D60303'], width: 240, height: 200, radius: 100
 end
 
 schedule do
-  send_event 'comms-odi-volume', current: NewsArticlesDashboard.volume(2014, "ODI")
+  send_event 'comms-odi-volume', current: NewsArticlesDashboard.volume(2016, "ODI")
 end
 
 schedule do
-  send_event 'comms-odi-value', current: NewsArticlesDashboard.value(2014, "ODI"), currency: "GBP"
+  send_event 'comms-odi-value', current: NewsArticlesDashboard.value(2016, "ODI"), currency: "GBP"
 end
 
 schedule do
-  send_event 'comms-odi-reach', current: NewsArticlesDashboard.reach(2014, "ODI")
+  send_event 'comms-odi-reach', current: NewsArticlesDashboard.reach(2016, "ODI")
 end
 
 schedule do
-  send_event 'comms-opendata-volume', current: NewsArticlesDashboard.volume(2014, "OpenData")
+  send_event 'comms-opendata-volume', current: NewsArticlesDashboard.volume(2016, "OpenData")
 end
 
 schedule do
-  send_event 'comms-opendata-value', current: NewsArticlesDashboard.value(2014, "OpenData"), currency: "GBP"
+  send_event 'comms-opendata-value', current: NewsArticlesDashboard.value(2016, "OpenData"), currency: "GBP"
 end
 
 schedule do
-  send_event 'comms-opendata-reach', current: NewsArticlesDashboard.reach(2014, "OpenData")
+  send_event 'comms-opendata-reach', current: NewsArticlesDashboard.reach(2016, "OpenData")
 end
 
 # 2015 Reach
